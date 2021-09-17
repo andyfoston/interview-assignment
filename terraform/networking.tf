0a99cac99e2874680a065616b398b85d0ee8a61b
Andy Foston <andy@foston.me>
2021-09-17 12:46:07 +0100

    Use HTTP health check instead of default TCP check

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -133,6 +133,14 @@ resource "aws_elb" "web" {
     lb_protocol       = "http"
   }
 
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
   instances                   = aws_instance.web[*].id
   subnets                     = aws_subnet.main[*].id
   security_groups             = [aws_security_group.elb.id]
